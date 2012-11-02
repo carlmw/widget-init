d25f63919cc3a917fd185c7f624389017387fe12
Carl Whittaker <carl.whittaker@unboxedconsulting.com>
2012-11-02 16:39:26 +0000

    Prevent definition at invocation

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = function (root) {
   var elements = root.querySelectorAll('[data-require]');
 
   for(var i = 0, l = elements.length; i < l; i++) initialiseWidget(elements[i]);
-  
-  function initialiseWidget (el) {
-    require('../' + el.getAttribute('data-require'))(el);
-  }
 };
+
+function initialiseWidget (el) {
+  require('../' + el.getAttribute('data-require'))(el);
+}
